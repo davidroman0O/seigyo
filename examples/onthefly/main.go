33c10485665bce36b4b291469a06c5c2b0760519
David Roman <romandavid83@gmail.com>
2023-10-10 23:13:52 -0400

    :fire: adding add and start on-the-fly

---
+++ examples/onthefly/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/davidroman0O/seigyo"
+)
+
+type Global struct{}
+
+func main() {
+
+	ctrl := seigyo.New(&Global{})
+	ctrl.RegisterProcess("basic", seigyo.ProcessConfig[*Global]{
+		Process: &BasicProcess{pid: "basic"},
+	})
+
+	errCh := ctrl.Start()
+
+	go func() {
+		// Add and start a new process on the fly.
+		if err := ctrl.AddAndStart("newProcess", seigyo.ProcessConfig[*Global]{
+			Process: &BasicProcess{pid: "newProcess"},
+		}); err != nil {
+			fmt.Println("Error starting new process:", err)
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Millisecond * 150)
+			shutdownSent, stopped, errored, err := ctrl.State("newProcess")
+			if err != nil {
+				fmt.Println("Error getting state of process:", err)
+				return
+			}
+			fmt.Println(shutdownSent, stopped, errored)
+		}
+	}()
+
+	go func() {
+		time.Sleep(time.Second * 1)
+		// Stop an individual process.
+		if err := ctrl.StopProcess("newProcess"); err != nil {
+			fmt.Println("Error stopping process:", err)
+		}
+	}()
+
+	for err := range errCh {
+		fmt.Println(err)
+	}
+
+	ctrl.Stop()
+	shutdownSent, stopped, errored, err := ctrl.State("newProcess")
+	if err != nil {
+		fmt.Println("Error getting state of process:", err)
+		return
+	}
+	fmt.Println(shutdownSent, stopped, errored)
+}
+
+// ExampleProcess is a simple implementation of the Process interface.
+type BasicProcess struct {
+	pid string
+}
+
+// Init initializes the process.
+func (p *BasicProcess) Init(ctx context.Context, stateGetter func() *Global, stateMutator func(mutateFunc func(*Global) *Global), sender func(pid string, data interface{})) error {
+	log.Println(p.pid, "initializing")
+	return nil
+}
+
+func (p *BasicProcess) Run(ctx context.Context, stateGetter func() *Global, stateMutator func(mutateFunc func(*Global) *Global), sender func(pid string, data interface{}), shutdownCh chan struct{}, errCh chan<- error) error {
+	log.Println(p.pid, "running")
+
+	for i := 0; i < 5; i++ {
+		log.Println(p.pid, "doing work", i)
+		time.Sleep(time.Millisecond * 150)
+	}
+
+	select {
+	case <-shutdownCh:
+		log.Println(p.pid, "received shutdown signal")
+	case <-ctx.Done():
+		log.Println(p.pid, "context done")
+	default:
+		log.Println(p.pid, "work done, exiting")
+	}
+
+	log.Println(p.pid, "finished")
+
+	return nil
+}
+
+// Deinit deinitializes the process.
+func (p *BasicProcess) Deinit(ctx context.Context, stateGetter func() *Global, stateMutator func(mutateFunc func(*Global) *Global), sender func(pid string, data interface{})) error {
+	log.Println(p.pid, "deinitializing")
+	return nil
+}
+
+// Received handles data received from another process.
+func (p *BasicProcess) Received(pid string, data interface{}) error {
+	log.Println(p.pid, "received data from", pid, ":", data)
+	return nil
+}
--- seigyo.go
+++ seigyo.go
@@ -9,6 +9,8 @@ import (
 	"time"
 )
 
+/// TODO: be able to start/stop process on the fly just like erlang
+
 // Process is an interface that describes a generic process with lifecycle methods
 // and communication capabilities.
 type Process[T any] interface {
@@ -192,108 +194,11 @@ func (c *Seigyo[T]) RegisterProcess(pid string, config ProcessConfig[T]) error {
 
 // Start initializes and runs all registered processes.
 func (c *Seigyo[T]) Start() chan error {
-	c.errCh = make(chan error, len(c.processes)) // Buffered to hold errors from all processes.
+	c.errCh = make(chan error, len(c.processes)*2) // Buffered to hold errors from all processes.
 
 	for pid, config := range c.processes {
 		c.wg.Add(1)
-
-		go func(pid string, config ProcessConfig[T]) {
-			defer c.wg.Done()
-
-			shutdownCh := c.shutdownChs[pid]
-			stateGetter := func() T {
-				c.stateMu.Lock()
-				defer c.stateMu.Unlock()
-				return c.state
-			}
-
-			stateMutator := func(mutateFunc func(T) T) {
-				c.stateMu.Lock()
-				defer c.stateMu.Unlock()
-				c.state = mutateFunc(c.state)
-			}
-
-			sender := func(targetPid string, data interface{}) {
-				log.Println("seding to", targetPid)
-				go func() {
-					if err := c.Send(pid, targetPid, data); err != nil {
-						c.errCh <- err
-					}
-				}()
-			}
-
-			// Define a helper function to execute a phase with optional panic recovery, retry, and timeout.
-			executePhase := func(phaseName string, phaseFunc func(context.Context) error, timeout time.Duration, maxRetries int, retryDelay time.Duration) error {
-				retries := 0
-				var lastErr error
-				for retries < maxRetries {
-					func() {
-						defer func() {
-							if r := recover(); r != nil {
-								lastErr = fmt.Errorf("panic during %s: %v", phaseName, r)
-								if config.ShouldRecover {
-									c.errCh <- fmt.Errorf("process %s: %w", pid, lastErr)
-								} else {
-									panic(r)
-								}
-							}
-						}()
-
-						// Create a context with an optional timeout.
-						ctx := context.Background()
-						if timeout > 0 {
-							var cancel func()
-							ctx, cancel = context.WithTimeout(ctx, timeout)
-							defer cancel()
-						}
-
-						lastErr = phaseFunc(ctx)
-					}()
-					if lastErr == nil {
-						return nil
-					}
-					retries++
-					time.Sleep(retryDelay)
-				}
-				return fmt.Errorf("error during %s: %w", phaseName, lastErr)
-			}
-
-			// Initialize the process with panic recovery, retry, and optional timeout.
-			if err := executePhase("initialization", func(ctx context.Context) error {
-				return config.Process.Init(ctx, stateGetter, stateMutator, sender)
-			}, config.InitTimeout, config.InitMaxRetries, config.InitRetryDelay); err != nil {
-				c.errCh <- fmt.Errorf("process %s: %w", pid, err)
-				c.stateMu.Lock()
-				c.errored[pid] = true
-				c.stateMu.Unlock()
-				return
-			}
-
-			// Run the process with panic recovery, retry, and optional timeout.
-			if err := executePhase("run", func(ctx context.Context) error {
-				return config.Process.Run(ctx, stateGetter, stateMutator, sender, shutdownCh, c.errCh)
-			}, config.RunTimeout, config.RunMaxRetries, config.RunRetryDelay); err != nil {
-				c.errCh <- fmt.Errorf("process %s: %w", pid, err)
-				c.stateMu.Lock()
-				c.errored[pid] = true
-				c.stateMu.Unlock()
-			}
-
-			// Deinitialize the process with panic recovery, retry, and optional timeout.
-			if err := executePhase("deinitialization", func(ctx context.Context) error {
-				return config.Process.Deinit(ctx, stateGetter, stateMutator, sender)
-			}, config.DeinitTimeout, config.DeinitMaxRetries, config.DeinitRetryDelay); err != nil {
-				c.errCh <- fmt.Errorf("process %s: %w", pid, err)
-				c.stateMu.Lock()
-				c.errored[pid] = true
-				c.stateMu.Unlock()
-			}
-
-			// Mark the process as stopped.
-			c.stateMu.Lock()
-			c.stopped[pid] = true
-			c.stateMu.Unlock()
-		}(pid, config)
+		go c.startProcess(pid, config)
 	}
 
 	// Close error channel once all processes have finished.
@@ -306,6 +211,172 @@ func (c *Seigyo[T]) Start() chan error {
 	}()
 
 	return c.errCh
+}
+
+func (c *Seigyo[T]) AddAndStart(pid string, config ProcessConfig[T]) error {
+	c.wg.Add(1)
+
+	c.stateMu.Lock()
+	defer c.stateMu.Unlock()
+
+	if config.InitMaxRetries == 0 {
+		config.InitMaxRetries = 1
+	}
+
+	if config.RunMaxRetries == 0 {
+		config.RunMaxRetries = 1
+	}
+
+	if config.DeinitMaxRetries == 0 {
+		config.DeinitMaxRetries = 1
+	}
+
+	if config.MessageSendMaxRetries == 0 {
+		config.MessageSendMaxRetries = 1
+	}
+
+	if config.InitRetryDelay == 0 {
+		config.InitRetryDelay = time.Nanosecond * 1
+	}
+
+	if config.RunRetryDelay == 0 {
+		config.RunRetryDelay = time.Nanosecond * 1
+	}
+
+	if config.DeinitRetryDelay == 0 {
+		config.DeinitRetryDelay = time.Nanosecond * 1
+	}
+
+	if _, exists := c.processes[pid]; exists {
+		return errors.New("process with pid already exists")
+	}
+
+	c.processes[pid] = config
+	c.shutdownChs[pid] = make(chan struct{})
+	c.shutdownSent[pid] = false
+	c.stopped[pid] = false
+	c.errored[pid] = false
+
+	// Start the new process.
+	go c.startProcess(pid, config)
+
+	return nil
+}
+
+func (c *Seigyo[T]) startProcess(pid string, config ProcessConfig[T]) {
+	defer c.wg.Done()
+
+	shutdownCh := c.shutdownChs[pid]
+	stateGetter := func() T {
+		c.stateMu.Lock()
+		defer c.stateMu.Unlock()
+		return c.state
+	}
+
+	stateMutator := func(mutateFunc func(T) T) {
+		c.stateMu.Lock()
+		defer c.stateMu.Unlock()
+		c.state = mutateFunc(c.state)
+	}
+
+	sender := func(targetPid string, data interface{}) {
+		log.Println("sending to", targetPid)
+		go func() {
+			if err := c.Send(pid, targetPid, data); err != nil {
+				c.errCh <- err
+			}
+		}()
+	}
+
+	// Define a helper function to execute a phase with optional panic recovery, retry, and timeout.
+	executePhase := func(phaseName string, phaseFunc func(context.Context) error, timeout time.Duration, maxRetries int, retryDelay time.Duration) error {
+		retries := 0
+		var lastErr error
+		for retries < maxRetries {
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						lastErr = fmt.Errorf("panic during %s: %v", phaseName, r)
+						if config.ShouldRecover {
+							c.errCh <- fmt.Errorf("process %s: %w", pid, lastErr)
+						} else {
+							panic(r)
+						}
+					}
+				}()
+
+				// Create a context with an optional timeout.
+				ctx := context.Background()
+				if timeout > 0 {
+					var cancel func()
+					ctx, cancel = context.WithTimeout(ctx, timeout)
+					defer cancel()
+				}
+
+				lastErr = phaseFunc(ctx)
+			}()
+			if lastErr == nil {
+				return nil
+			}
+			retries++
+			time.Sleep(retryDelay)
+		}
+		return fmt.Errorf("error during %s: %w", phaseName, lastErr)
+	}
+
+	// Initialize the process with panic recovery, retry, and optional timeout.
+	if err := executePhase("initialization", func(ctx context.Context) error {
+		return config.Process.Init(ctx, stateGetter, stateMutator, sender)
+	}, config.InitTimeout, config.InitMaxRetries, config.InitRetryDelay); err != nil {
+		c.errCh <- fmt.Errorf("process %s: %w", pid, err)
+		c.stateMu.Lock()
+		c.errored[pid] = true
+		c.stateMu.Unlock()
+		return
+	}
+
+	// Run the process with panic recovery, retry, and optional timeout.
+	if err := executePhase("run", func(ctx context.Context) error {
+		return config.Process.Run(ctx, stateGetter, stateMutator, sender, shutdownCh, c.errCh)
+	}, config.RunTimeout, config.RunMaxRetries, config.RunRetryDelay); err != nil {
+		c.errCh <- fmt.Errorf("process %s: %w", pid, err)
+		c.stateMu.Lock()
+		c.errored[pid] = true
+		c.stateMu.Unlock()
+	}
+
+	// Deinitialize the process with panic recovery, retry, and optional timeout.
+	if err := executePhase("deinitialization", func(ctx context.Context) error {
+		return config.Process.Deinit(ctx, stateGetter, stateMutator, sender)
+	}, config.DeinitTimeout, config.DeinitMaxRetries, config.DeinitRetryDelay); err != nil {
+		c.errCh <- fmt.Errorf("process %s: %w", pid, err)
+		c.stateMu.Lock()
+		c.errored[pid] = true
+		c.stateMu.Unlock()
+	}
+
+	// Mark the process as stopped.
+	c.stateMu.Lock()
+	c.stopped[pid] = true
+	c.stateMu.Unlock()
+}
+
+func (c *Seigyo[T]) StopProcess(pid string) error {
+	c.stateMu.Lock()
+	defer c.stateMu.Unlock()
+
+	// Check if the process exists.
+	if _, exists := c.processes[pid]; !exists {
+		return errors.New("process with pid does not exist")
+	}
+
+	// Signal the process to shut down if it hasn't been signaled yet.
+	if !c.shutdownSent[pid] {
+		close(c.shutdownChs[pid])
+		c.shutdownSent[pid] = true
+	}
+
+	return nil
 }
 
 // Stop stops all registered processes.
